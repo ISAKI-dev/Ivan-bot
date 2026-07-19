8479e5c5c2e27661a7b15e5d1ba075f23d6a07cb
ISAKI-dev <celovekov124@gmail.com>
2026-07-19 12:39:47 +0300

    Update run.py

--- run.py
+++ run.py
@@ -14,6 +14,10 @@ dp = Dispatcher()
 async def cmd_start(massage: Message):
     await massage.answer("Привет!")
 
+dp.message(Command("help"))
+async def id(massange:Message):
+    await massange.reply(f"Привет. \nТвой ID: {massange.from_user.id}\nИмя: {massange.from_user.first_name}")
+
 @dp.message(Command("help"))
 async def get_help(massange:Message):
     await massange.answer("Привет этот бот пока ничего не умеет((")
@@ -22,9 +26,14 @@ async def get_help(massange:Message):
 async def how_are_ivan(massange: Message):
     await massange.answer("У Ивана все прекрасно")
 
-@dp.message(F.photo)
+@dp.message(Command("get_photo"))
 async def get_photo(massange:Message):
     await massange.answer(f"ID фото: {massange.photo[-1].file_id}")
+
+@dp.message(F.photo)
+async def get_photo(massange:Message):
+    await massange.answer(photo = "AgACAgIAAxkBAAMPalyYc_q06y3jwtuyaq36x3uZ3_gAAuMVaxtU4OFK6yPgBa_J6FoBAAMCAAN4AAM9BA"
+                          , caption="Это ваше фото")
 
 async def main():
     await dp.start_polling(bot)
@@ -32,6 +41,9 @@ async def main():
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Exit")
         asyncio.run(main())
     except KeyboardInterrupt:
         print("Exit")
